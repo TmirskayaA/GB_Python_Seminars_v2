66d94892a3021a62945977270ecc48e02a9ac97c
Tmirskaya <twodragons@mail.ru>
2023-05-21 14:04:06 +0300

    task4

---
+++ Seminar_1/Task_4.py
@@ -0,0 +1,16 @@
+print("Здравствуйте!")
+long = int(input("Какая у шоколадки длина? "))
+width = int(input("Какая ширина? "))
+counter = int(input("Сколько долек хотите отломить? "))
+
+if counter > long * width:
+    print("Чтобы отломить столько долек, вам нужна шоколадка побольше.")
+
+def CanIGetTheseSlices(x, y, z):
+    if z % x == 0 or z % y == 0:
+        return True
+    else: return False
+
+if CanIGetTheseSlices(long, width, counter):
+    print("Да, всё получится!")
+else: print("Нет, так ничего не выйдет...")
