6915b7a6f215ad99e45060190dcb37994b5f594d
Tmirskaya <twodragons@mail.ru>
2023-06-05 22:49:35 +0300

    s3t3

--- draft_for_test.py
+++ draft_for_test.py
@@ -1,20 +1,8 @@
-a = [1, 2, 3, 4, 5]
-min = 1
-i = 4
-l = 0
-while i >= min:
-    for j in a:
-        if j == i: 
-            l = i
-            print(f"L = {l}")
-        i -= 1
 
-a = [5,4,3,1,2]
-i = 1
-max = 5
-while i <= max:
-    for j in a:
-        if j == i: 
-            l = i
-            print(f"M = {l}")
-    i += 1
+
+array1 = list(str(input()))
+array2 = list(str(input()))
+
+p = set(array1).intersection(array2)
+
+print(len(p))
